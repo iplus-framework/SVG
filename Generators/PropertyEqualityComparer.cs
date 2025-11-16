41a5c8cd2ef68f12d68211c6b9b565b379d6dfee
Damir Lisak <dlisak@kajbum.com>
2025-11-16 21:58:19 +0100

    Compilation with avalonia and iplus sources enabled + Central package management.

--- Generators/PropertyEqualityComparer.cs
+++ Generators/PropertyEqualityComparer.cs
@@ -9,14 +9,20 @@ namespace Svg.Generators
     class PropertyEqualityComparer : IEqualityComparer<Property>
     {
         /// <inheritdoc/>
-        public bool Equals(Property p1, Property p2)
+        public bool Equals(Property? p1, Property? p2)
         {
+            if (p1 == null || p2 == null)
+                return p1 == null && p2 == null;
+            
             return SymbolEqualityComparer.Default.Equals(p1.Symbol, p2.Symbol);
         }
 
         /// <inheritdoc/>
         public int GetHashCode(Property p)
         {
+            if (p == null)
+                return 0;
+                
 #pragma warning disable RS1024
             return p.Symbol.GetHashCode();
 #pragma warning restore RS1024
